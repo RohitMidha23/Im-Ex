6b29ddf40c79fd2c52e830ffef55cf5f18216aaf
Rohit Midha <rohit.midha23@gmail.com>
2018-05-27 17:01:22 +0530

    Initial Commit

---
+++ Im-Ex/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Im-Ex
+//
+//  Created by Rohit  on 27/05/2018.
+//  Copyright © 2018 Rohit . All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
